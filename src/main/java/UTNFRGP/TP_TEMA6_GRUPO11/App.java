64e8dfd072500835cc1d668e9c270285d55e9df0
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-04-30 14:26:36 -0300

    se sube #TK1 #US1 realizada con exito

---
+++ src/main/java/UTNFRGP/TP_TEMA6_GRUPO11/App.java
@@ -0,0 +1,13 @@
+package UTNFRGP.TP_TEMA6_GRUPO11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
