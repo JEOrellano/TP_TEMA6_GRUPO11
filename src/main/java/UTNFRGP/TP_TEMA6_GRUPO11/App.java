14215355444a7b65cace8a17370ec594366d6663
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-05-01 17:35:13 -0300

    #TK2 #US1 realizada con exito

--- src/main/java/UTNFRGP/TP_TEMA6_GRUPO11/App.java
+++ src/main/java/UTNFRGP/TP_TEMA6_GRUPO11/App.java
@@ -1,9 +1,5 @@
 package UTNFRGP.TP_TEMA6_GRUPO11;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/entidad/Medico.java
@@ -0,0 +1,157 @@
+package entidad;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import enums.Sexo;
+
+@Entity
+@Table(name="Medico")
+public class Medico implements Serializable{
+	// Implementar serializable
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="legajo",unique=true)
+	private String legajo;
+	
+	@Column(name="nombre")
+	private String nombre;
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="sexo")
+	private Sexo sexo;
+	
+	@Column(name="nacimiento")
+	private LocalDate nacimiento;
+	
+	@Column(name="direccion")
+	private String direccion;
+	
+	@Column(name="localidad")
+	private String localidad;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="telefono")
+	private String telefono;
+	
+	// constructor vacio
+	public Medico() {
+	}
+
+	public Medico(int id, String legajo, String nombre, String apellido, Sexo sexo, LocalDate nacimiento,
+			String direccion, String localidad, String email, String telefono) {
+		this.id = id;
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.sexo = sexo;
+		this.nacimiento = nacimiento;
+		this.direccion = direccion;
+		this.localidad = localidad;
+		this.email = email;
+		this.telefono = telefono;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public LocalDate getNacimiento() {
+		return nacimiento;
+	}
+
+	public void setNacimiento(LocalDate nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	@Override
+	public String toString() {
+		return "Medico [id=" + id + ", legajo=" + legajo + ", nombre=" + nombre + ", apellido=" + apellido + ", sexo="
+				+ sexo + ", nacimiento=" + nacimiento + ", direccion=" + direccion + ", localidad=" + localidad
+				+ ", email=" + email + ", telefono=" + telefono + "]";
+	}
+}
