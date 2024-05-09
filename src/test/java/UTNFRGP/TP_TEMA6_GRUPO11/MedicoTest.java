1add6bf0cd6cee9952154bbabe338a0eaf5e54e5
gaston5050 <80988908+gaston5050@users.noreply.github.com>
2024-05-09 16:53:58 -0300

    JUNIT caminando?

---
+++ src/test/java/UTNFRGP/TP_TEMA6_GRUPO11/MedicoTest.java
@@ -0,0 +1,16 @@
+package UTNFRGP.TP_TEMA6_GRUPO11;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public  class MedicoTest {
+	
+	@Test
+	public  void beforeAllTest()
+	{
+		System.out.println("Before all");
+	}
+
+	
+}
